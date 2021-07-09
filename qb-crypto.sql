3923bc96fd747c5cb1b2288fed062317c4a0ebe4
GhzGarage <gigahertzgarage@mail.com>
2021-07-08 19:54:27 -0500

    ⚠️⚠️ Steam Ditch ⚠️⚠️

--- qb-crypto.sql
+++ qb-crypto.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `crypto` (
-  `#` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `crypto` varchar(50) NOT NULL DEFAULT 'qbit',
   `worth` int(11) NOT NULL DEFAULT 0,
   `history` text DEFAULT NULL,
-  PRIMARY KEY (`#`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `crypto` VALUES (1, 'qbit', 0, '[{"NewWorth":0,"PreviousWorth":0}]');
