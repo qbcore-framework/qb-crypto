b6a99077fed6cd59997fdbd32abd9bda53c5e4d3
Deltarix <32911010+RealDeltarix@users.noreply.github.com>
2021-07-30 17:38:14 +0200

    config for new value timer

--- config.lua
+++ config.lua
@@ -12,6 +12,8 @@ Crypto.Labels = {
     ["qbit"] = "Qbit"
 }
 
+Crypto.NewValue = 10 -- In minutes, so every 10 minutes.
+
 Crypto.Exchange = {
     coords = vector3(1276.21, -1709.88, 54.57),
     RebootInfo = {
