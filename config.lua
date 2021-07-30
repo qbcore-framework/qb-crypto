cea187db33b1e788cdf57c798d55fffdf1a7e1ce
Deltarix <32911010+RealDeltarix@users.noreply.github.com>
2021-07-30 19:48:47 +0200

    update to config with crypto update

--- config.lua
+++ config.lua
@@ -1,21 +1,38 @@
-Crypto = Crypto or {}
+Crypto = {
 
-Crypto.History = {
-    ["qbit"] = {}
-}
+    History = {
+        ["qbit"] = {}
+    },
 
-Crypto.Worth = {
-    ["qbit"] = 0
-}
+    Worth = {
+        ["qbit"] = 0
+    },
+    
+    Labels = {
+        ["qbit"] = "Qbit"
+    },
 
-Crypto.Labels = {
-    ["qbit"] = "Qbit"
-}
+    Exchange = {
+        coords = vector3(1276.21, -1709.88, 54.57),
+        RebootInfo = {
+            state = false,
+            percentage = 0
+        },
+    },
 
-Crypto.Exchange = {
-    coords = vector3(1276.21, -1709.88, 54.57),
-    RebootInfo = {
-        state = false,
-        percentage = 0
-    }
+
+    -- For auto updating the value of qbit
+    Coin = 'qbit',
+    NewValue = 10, -- In minutes, so every 10 minutes.
+
+    -- Crashes or luck
+    ChanceOfCrashOrLuck = 10, -- This is in % (1-100)
+    Crash = {100,1000}, -- Min / Max
+    Luck = {100,250}, -- Min / Max
+
+    -- If not not Chance of crash or luck, then this shit
+    ChanceOfDown = 30, -- If out of 100 hits less or equal to
+    ChanceOfUp = 60, -- If out of 100 is greater or equal to
+    CasualDown = {1,49}, -- Min / Max (If it goes down)
+    CasualUp = {1,49}, -- Min / Max (If it goes up)
 }
