d3665912b3bce345ba7fcc84c9422cad23311e80
Sigurd <81210348+SjurdVrancken@users.noreply.github.com>
2024-08-17 12:58:26 +0200

    feat(locales): add .nl locales

---
+++ locales/nl.lua
@@ -0,0 +1,22 @@
+local Translations = {
+    menus = {
+        header = 'Crafting Menu',
+        pickupworkBench = 'Pak werkbank op',
+        entercraftAmount = 'Voer hoeveelheid in:',
+    },
+    notifications = {
+        pickupBench = 'Je hebt je werkbank opgepakt.',
+        invalidAmount = 'Ongeldige hoeveelheid',
+        invalidInput = 'Ongeldige invoer',
+        notenoughMaterials = "Je hebt niet genoeg materialen!",
+        craftingCancelled = 'Je hebt je crafting geannuleerd',
+        tablePlace = 'Je hebt je werkbank geplaatst',
+        craftMessage = 'Je hebt een %s gemaakt',
+        xpGain = 'Je hebt %d XP verkregen %s',
+    }
+}
+
+Lang = Lang or Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
